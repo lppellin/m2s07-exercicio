97857110d9b53a1ff078c18e8cd8e7907c93ba74
lppellin <lucaspaolopellin@gmail.com>
2025-01-25 09:55:32 -0300

    init - config express / typeorm

---
+++ src/data-source.ts
@@ -0,0 +1,16 @@
+import "reflect-metadata"
+import { DataSource } from "typeorm"
+
+export const AppDataSource = new DataSource({
+    type: "postgres",
+    host: "localhost",
+    port: 5432,
+    username: "postgres",
+    password: "0182",
+    database: "m2s07-trello",
+    synchronize: true,
+    logging: false,
+    entities: ["/src/entity/*.ts"],
+    migrations: ["src/migration/*.ts"],
+    subscribers: [],
+})
