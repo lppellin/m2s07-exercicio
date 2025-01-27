d24d3b04d891b575a29a9eef108ece628285fea6
lppellin <lucaspaolopellin@gmail.com>
2025-01-26 23:01:04 -0300

    fix rota get e get/all - paginação

--- src/routes/medicamentos.routes.ts
+++ src/routes/medicamentos.routes.ts
@@ -28,11 +28,57 @@ medicamentosRouter.post("/", authToken, async (req: Request, res: Response) => {
   }
 });
 
-medicamentosRouter.get("/", authToken, async (req: Request, res: Response) => {
+medicamentosRouter.get("/", async (req: Request, res: Response) => {
   try {
-    const medicamentos = await medicamentoRepository.find();
+    const userId = Number(req.headers.userid);
 
-    res.status(200).json(medicamentos);
+    if (!userId) {
+      res.status(400).json("Será necessário informar o userId no header");
+      return;
+    }
+
+    const page = Number(req.query.page ?? 1);
+    const limit = Number(req.query.limit ?? 10);
+
+    const skip = page > 1 ? (page - 1) * limit : 0;
+
+    const result = await medicamentoRepository.find({
+      where: {
+        userId: userId,
+      },
+      skip: skip,
+      take: limit,
+    });
+
+    if (!result) {
+      res.status(200).json("Nenhum medicamento encontrado!");
+      return;
+    }
+
+    res.status(200).json(result);
+  } catch (ex) {
+    res.status(500).json("Não foi possível executar a solicitação!");
+  }
+});
+
+medicamentosRouter.get("/all", async (req: Request, res: Response) => {
+  try {
+    const page = Number(req.query.page ?? 1);
+    const limit = Number(req.query.limit ?? 10);
+
+    const skip = page > 1 ? (page - 1) * limit : 0;
+
+    const result = await medicamentoRepository.find({
+      skip: skip, // 20 - 30
+      take: limit,
+    });
+
+    if (!result) {
+      res.status(200).json("Nenhum medicamento encontrado!");
+      return;
+    }
+
+    res.status(200).json(result);
   } catch (ex) {
     res.status(500).json("Não foi possível executar a solicitação!");
   }
