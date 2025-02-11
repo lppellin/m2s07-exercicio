523f3361e4e85cc1459b149d7f32a10c4f1bdfbc
lppellin <lucaspaolopellin@gmail.com>
2025-02-10 22:25:41 -0300

    implementa jwt token

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,11 +1,21 @@
 import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
 
 const authToken = async (req: Request, res: Response, next: NextFunction) => {
-  if (req.headers.senha == "1234") {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    if (!token) {
+      res.status(401).json("Acesso não autorizado");
+      return;
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
+    req.body.userId = decoded;
+
     next();
-    return;
+  } catch (error) {
+    res.status(401).json("Token inválido");
   }
-  res.status(401).json("Acesso negado!");
 };
 
 export default authToken;
